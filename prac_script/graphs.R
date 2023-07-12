4240190d59231e61562962fb9e67543aace36fb8
eparker12 <edward.parker@lshtm.ac.uk>
2023-07-12 13:50:56 +0100

    Fig 2 example code

--- prac_script/graphs.R
+++ prac_script/graphs.R
@@ -7,28 +7,50 @@ library(tidyverse)
 library(readxl)
 
 
+# Check local working directory
+getwd()
+
 # Import files 
-Combined_comorbidity_age_region <- read_excel("~/Documents/GitHub/summer_thesis/data/Combined_comorbidity_age_region.xlsx")
-bmi_cat <- read_excel("~/Documents/GitHub/summer_thesis/data/bmi_cat.xlsx")
-ethnicity <- read_excel("~/Documents/GitHub/summer_thesis/data/ethnicity.xlsx")
-sm_cat <- read_excel("~/Documents/GitHub/summer_thesis/data/sm_cat.xlsx")
+Combined_comorbidity_age_region <- read_excel("../Data/Combined_comorbidity_age_region.xlsx")
+bmi_cat <- read_excel("../Data/bmi_cat.xlsx")
+ethnicity <- read_excel("../Data/ethnicity.xlsx")
+sm_cat <- read_excel("../Data/sm_cat.xlsx")
+
 # Rename file 
 data <- Combined_comorbidity_age_region
 
-
 # Run saved script in showcase mode
-runApp("~/Documents/GitHub/summer_thesis/shiny_scripts", display.mode = "showcase")
+#runApp("~/Documents/GitHub/summer_thesis/shiny_scripts", display.mode = "showcase")
 
+# Summarise data
+summary(data)
 
-# Summarise 'data' 
-sapply(data, function(x) sapply(x, class))
+# Convert age_group and region_cat from 'character' to factor column type 
+data$age_group <- as.factor(data$age_group)
+
+# Fig 2 first panel plots cancer prevalence by age in 2019
+## Filter data to relevant subset
+subset <- data %>%
+  filter(year==2019 & comorbidity=="cancerlastyr" &
+           region_cat %in% c("Scotland", "Wales", "Northern Ireland", "West Midlands"))
+# West Midlands included as combined 'England' data not extracted in current data
+
+## Generate plot
+g1 = ggplot(subset, aes(x=age_group, y=comorbidity_prop, colour = region_cat, group = region_cat)) + # Covariates cannot be in " " in this line!
+  geom_line() +
+  ggtitle("Cancer (previous 1y)") + 
+  theme_bw() +
+  xlab("Age (years) : 2-9 yrs; 5 year age bands; 90-99 yrs") +
+  ylab("Prevalence/100,000") +
+  labs(colour="") +
+  theme(axis.text.x=element_text(angle = 45, hjust = 1))
+g1
+# Print graph 
 
 
 # Change comorbidity_yes column from character to numeric 
 # Some values (<5) have been converted to NA
 data$comorbidity_yes <- as.numeric(data$comorbidity_yes)
-# Convert age_group and region_cat from 'character' to factor column type 
-data$age_group <- as.factor(data$age_group)
 data$region_cat <- as.factor(data$region_cat)
 data$comorbidity <- as.factor(data$comorbidity)
 
@@ -42,8 +64,8 @@ table(data$comorbidity)
 data$comorbidity[data$comorbidity == 'bmi_40'] <- 'bmi40'
 
 # Create histogram for morbidities with age group 
-p1 <- ggplot(data, aes(x="age_group", y="comorbidity_yes", colour = 'region_cat')) +
-  geom_histogram() +
+ggplot(data, aes(x="age_group", y="comorbidity_yes")) +
+  geom_boxplot() +
   ggtitle("Morbidity Prevalance by Age Group")
 # Print graph 
 p1
